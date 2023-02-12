2410b5f37c34ca0ec61921f72e9b08173c1a95ae
Emirhan DOĞRU <emirhan-dogru@hotmail.com>
2023-02-12 16:06:17 +0300

    tema parçalandı , htaccess entegre edildi

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,10 @@
+<div class="wrap p-t-0">
+  <footer class="app-footer">
+    <div class="clearfix">
+      <ul class="footer-menu pull-right">
+
+      </ul>
+      <div class="copyright pull-left">Copyright Emirhan Doğru &copy; <?= date('Y') ?></div>
+    </div>
+  </footer>
+</div>
